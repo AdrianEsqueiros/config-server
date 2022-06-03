a3ede7f20646b5cd357a91e76730930544448788
Adrian <u201718169@upc.edu.pe>
2022-06-03 18:48:31 -0500

    Initial commit

---
+++ src/test/java/com/goback/restore/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goback.restore.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
